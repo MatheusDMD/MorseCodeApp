cee546de6fe6e72ccd22925a072eae9502b740a4
MarceloHarad <marcelogdeandrade@gmail.com>
2016-04-15 16:10:01 -0300

    Fixing bug try 1

---
+++ app/src/main/java/agile/app/morsecodeapp/MorseTouch.java
@@ -0,0 +1,13 @@
+package agile.app.morsecodeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MorseTouch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_morse_touch);
+    }
+}
