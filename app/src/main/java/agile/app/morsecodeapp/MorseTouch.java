83dd624642bcbe0985ab3d325fbdc40efb19e8ed
matheusdmd <matheusdmd@al.insper.edu.br>
2016-04-21 17:26:11 -0300

    adding button + sending sms func

--- app/src/main/java/agile/app/morsecodeapp/MorseTouch.java
+++ app/src/main/java/agile/app/morsecodeapp/MorseTouch.java
@@ -10,6 +10,23 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,11 +49,18 @@ public class MorseTouch extends AppCompatActivity {
     private String phrase;
     private TextView phraseView;
     private TextView phraseViewTouch;
+    private TextView phoneText;
+    private Button sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_morse_touch);
+
+        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS);
+        if(permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, 0);
+        }
 
         this.startTimeDown = 0;
         this.startTimeUp = 0;
@@ -45,20 +69,36 @@ public class MorseTouch extends AppCompatActivity {
         this.morseText = new ArrayList<String>();
         this.decoder = new Decoder();
         this.phrase = "";
-        this.phraseView = (TextView)findViewById(R.id.textMorse);
-        this.phraseViewTouch = (TextView)findViewById(R.id.touchWarning);
+        this.phraseView = (TextView) findViewById(R.id.textMorse);
+        this.phraseViewTouch = (TextView) findViewById(R.id.textMorse);
+        this.phoneText = (TextView) findViewById(R.id.textPhone);
+        this.sendButton = (Button) findViewById(R.id.sendButton);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SmsManager manager = SmsManager.getDefault();
+                String phone = phoneText.getText().toString();
+                if(PhoneNumberUtils.isWellFormedSmsAddress(phone)) {
+                    manager.sendTextMessage(phone, null, phrase, null, null);
+                    //Toast.makeText(MorseTouch.this, "Torpedo enviado!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    //Toast.makeText(MainActivity.this, "Número inválido!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
-    public boolean onTouchEvent(MotionEvent event){
+    public boolean onTouchEvent(MotionEvent event) {
 
-
-
-        if (!startRecording){
+        if (!startRecording) {
             this.startRecording = true;
             phraseViewTouch.setText("");
         }
-        if (startRecording){;
-            if(event.getAction() == MotionEvent.ACTION_DOWN) {
+        if (startRecording) {
+            ;
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
                 setActivityBackgroundColor(Color.parseColor("#2C2C2C"));
                 this.startTimeDown = 0;
                 this.timeDown = 0;
@@ -67,14 +107,14 @@ public class MorseTouch extends AppCompatActivity {
                 timeUp = System.currentTimeMillis() - startTimeUp;
             }
 
-            if(event.getAction() == MotionEvent.ACTION_UP) {
+            if (event.getAction() == MotionEvent.ACTION_UP) {
                 setActivityBackgroundColor(Color.parseColor("#E0E0E0"));
                 timeDown = System.currentTimeMillis() - startTimeDown;
                 startTimeUp = System.currentTimeMillis();
             }
         }
         if (timeDown > 0 && timeUp > 0) {
-            if (timeDown > 5 * unit){
+            if (timeDown > 5 * unit) {
                 Log.e("MorseTouch", "EndPhrase");
                 morseText.add(" ");
                 phrase += decoder.decodeMorse(morseText);
@@ -82,8 +122,7 @@ public class MorseTouch extends AppCompatActivity {
                 phraseView.setText(phrase);
                 Log.e("MorseTouch", phrase);
                 morseText.clear();
-            }
-            else {
+            } else {
                 //End Word
                 if (timeUp > 6 * unit && timeUp < 100 * unit) {
                     Log.e("MorseTouch", "EndWord");
